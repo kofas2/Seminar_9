9ca2340b4310781f6bb20a9ecc3162d810d5ee34
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-04-09 11:57:53 +0300

    add Task004 Seminar 9

---
+++ Task004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр введенного числа {SumNumbers(a)}");
+
+int SumNumbers(int a)
+{
+    if (a==0) return 0;
+    return a%10 + SumNumbers(a/10);
+}
